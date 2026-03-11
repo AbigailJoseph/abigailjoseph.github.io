ba22b71a0051b6691b3af8bfcefb9cbf31d40f17
Abigail Joseph <abigailmj1905@gmail.com>
2026-03-11 10:39:53 -0400

    update layout, fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
   return (
     <footer className="bg-[#f6eef7] border-t border-[#7f2650]/10 py-6 text-center">
-      <p className="text-[#7f2650]/50 text-sm">made with &hearts;</p>
       <p className="text-[#7f2650]/50 text-sm">₊✩‧₊˚౨ৎ˚₊✩‧₊</p>
+      <p className="text-[#7f2650]/50 text-sm">made with &hearts; by me</p>
+      <p className="text-[#7f2650]/50 text-sm">© 2026 Abigail Joseph</p>
+      
     </footer>
   );
 }
